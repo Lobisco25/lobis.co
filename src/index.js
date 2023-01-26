b29231731c34152a07aff0f73715fe22fb2aa3e3
Lobisco25 <fefeeliaslobo@outlook.com>
2023-01-26 17:46:15 -0300

    support for incidentbot website

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const path = require("path")
 
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer();
+
 const public = path.join(__dirname, "public")
 
 const app = express()
@@ -8,10 +12,12 @@ const app = express()
 const port = 9004
 
 app.use(express.static(public))
-console.log(public)
 
 app.get("/", (req, res) => {
      res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+app.use('/bot', (req, res) => {
+     proxy.web(req, res, { target: 'http://localhost:9010' });
+ })
 app.listen(port)
